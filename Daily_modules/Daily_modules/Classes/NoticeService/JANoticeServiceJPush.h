fdbd75b9555c641e5dd07f1876fb88c105e4ad9b
Jason <iJason92@yahoo.com>
2017-01-10 07:42:49 +0800

    Daily_modules_1484005357

--- Daily_modules/Daily_modules/Classes/NoticeService/JANoticeServiceJPush.h
+++ Daily_modules/Daily_modules/Classes/NoticeService/JANoticeServiceJPush.h
@@ -27,7 +27,6 @@ UIKIT_EXTERN NSString *const kJAServiceErrorNotification;              // 错误
  * @param appKey 一个JPush 应用必须的,唯一的标识. 请参考 JPush 相关说明文档来获取这个标识.
  * @param channel 发布渠道. 可选.
  * @param isProduction 是否生产环境. 如果为开发状态,设置为 NO; 如果为生产状态,应改为 YES.
- * @param advertisingIdentifier 广告标识符（IDFA） 如果不需要使用IDFA，传nil.
  *
  * @discussion 提供SDK启动必须的参数, 来启动 SDK.
  * 此接口必须在 App 启动时调用, 否则 JPush SDK 将无法正常工作.
@@ -37,7 +36,18 @@ UIKIT_EXTERN NSString *const kJAServiceErrorNotification;              // 错误
                 channel:(NSString *)channel
        apsForProduction:(BOOL)isProduction;
 
-
+/*!
+ * @abstract 启动SDK
+ *
+ * @param launchingOption 启动参数.
+ * @param appKey 一个JPush 应用必须的,唯一的标识. 请参考 JPush 相关说明文档来获取这个标识.
+ * @param channel 发布渠道. 可选.
+ * @param isProduction 是否生产环境. 如果为开发状态,设置为 NO; 如果为生产状态,应改为 YES.
+ * @param advertisingId 广告标识符（IDFA） 如果不需要使用IDFA，传nil.
+ *
+ * @discussion 提供SDK启动必须的参数, 来启动 SDK.
+ * 此接口必须在 App 启动时调用, 否则 JPush SDK 将无法正常工作.
+ */
 + (void)setupWithOption:(NSDictionary *)launchingOption
                  appKey:(NSString *)appKey
                 channel:(NSString *)channel
@@ -45,6 +55,12 @@ UIKIT_EXTERN NSString *const kJAServiceErrorNotification;              // 错误
   advertisingIdentifier:(NSString *)advertisingId;
 
 
+/**
+ 注册极光通知的事件
+
+ @param notificationEvent 极光通知的事件
+ @param execBlock 事件对应的回调block
+ */
 - (void)regJPushNotication:(NSString *)notificationEvent
                  execBlock:(void (^)(NSNotification *notification))execBlock;
 
